ce22163fdc6089112edde1efddd90eb948dde33f
Tuy RAVY <tuyravey99@gmail.com>
2018-11-26 13:30:08 +0700

    no message

--- application/views/daily/RM/LoanPortfolio.php
+++ application/views/daily/RM/LoanPortfolio.php
@@ -91,7 +91,11 @@
                           <td style="text-align:right" colspan='2'>Total:</td>                           
                               <td style="text-align:right"><?php echo number_format($toalbalamt,0);?></td>
                               <td style="text-align:left" colspan='3'></td>
-                          </tr>                            
+                          </tr>     
+                          <td style="text-align:right" colspan='2'>Total USD:</td>                           
+                              <td style="text-align:right"><?php echo number_format($toalbalamt/4000,0);?></td>
+                              <td style="text-align:left" colspan='3'></td>
+                          </tr>                       
                         </table>
                         <!-- <div class="pull-right">
                           <div style="margin-top: 25px;margin-bottom: -12px;">
