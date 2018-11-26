f20e07e1a9f74a67430ba98be414c447f5c98074
Tuy RAVY <tuyravey99@gmail.com>
2018-11-26 13:06:14 +0700

    no message

--- application/config/database.php
+++ application/config/database.php
@@ -120,3 +120,28 @@ $db['default'] = array(
 // 	'save_queries' => TRUE
 // );
 
+// $active_group = 'default';
+// $query_builder = TRUE;
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'spmficom_cbs',
+// 	'password' => 'St4N+!%pZoL%',
+// 	'database' => 'spmficom_cbs_reporting',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
