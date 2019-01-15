fab844dec7ea2aa8642dcf87264775d1e53e936d
Tuy RAVY <tuyravey99@gmail.com>
2019-01-15 12:09:55 +0700

    no message

--- application/views/daily/DCEO/Performentbyco.php
+++ application/views/daily/DCEO/Performentbyco.php
@@ -159,7 +159,7 @@
                                         <td><?=  number_format($row->PAR1Days,0);$TotalPar1Days+=$row->PAR1Days;?></td>
                                         <td><?=  number_format($row->PAR7Days,0);$TotalPar7Days+=$row->PAR7Days;?></td>
                                         <td><?=  number_format($row->PAR30Days,0);$TotalPar30Days+=$row->PAR30Days;?></td>
-                                        <td><?php if($row->PAR1Days==0 || $row->Balance==0 ){ $PAR1DAY=0;$Balance=0; echo 0;}else{ echo number_format($row->PAR1Days/$row->Balance*100,2);}?>%</td>
+                                        <td><?php if($row->PAR1Days==0 || $row->Balance==0 ){ $PAR1DAY=0;$Balance=0; echo 0;}else{ echo number_format(empty($row->PAR1Days)/$row->Balance*100,2);}?>%</td>
                                         <td><?=  number_format($row->DisbAmt,0);$TotalDisbAmt+=$row->DisbAmt;?></td>
                                         <td><?=  $row->ClientDisb;$TotalClientDisb+= $row->ClientDisb;?></td>
 
@@ -180,7 +180,7 @@
                                         <td><?php if($row->PAR1Days==0 || $row->PAR1Days_pre==0 || $row->Balance_pre==0 ||$row->Balance==0){ $PAR1Days=1;$PAR1Days_pre=1;$Balance_pre=1;$Balance=1;echo number_format(($PAR1Days/$Balance-$PAR1Days_pre/$Balance_pre)*100,2);}else{echo number_format(($row->PAR1Days/$row->Balance-$row->PAR1Days_pre/$row->Balance_pre)*100,2);}?>%</td>
                                         <td><?=  number_format(($row->DisbAmt)-($row->DisbAmt_pre),0);?></td>
                                         <td><?=  ($row->ClientDisb)-($row->ClientDisb_pre);?></td>
-                                        <td style="text-align:left;"><?=  $row->shortcode;?></td>
+                                        <td style="text-align:left;"><?=  $row->BrName;?></td>
                                         <td><?=  $row->BrCode;?></td>
                                     </tr>
                                 <?php endforeach;?>
